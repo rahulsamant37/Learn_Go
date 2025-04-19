10fb275204277b2b3dfec5d58ec444596a630ddb
Rahul Samant <161972011+rahulsamant37@users.noreply.github.com>
2025-04-19 23:33:11 +0000

    feat: trying variable with strictly define syntax, shorthand syntax to define variables in go

---
+++ 03_variables/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "golang"
+
+	// auto infer
+	var name = "golang2"
+	var isAdult = true
+
+	//shorthand syntax
+	name2 := "golang"
+
+	fmt.Println(name)
+	fmt.Println(isAdult)
+	fmt.Println(name2)
+}
