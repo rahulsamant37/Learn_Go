d5d4a98667a54767c80b1daa478e94125b2b8fcb
Rahul Samant <rahulsamantcoc2@gmail.com>
2025-08-11 13:25:05 +0000

    feat: Add constant declaration for port and host configuration

--- 04_constants/constants.go
+++ 04_constants/constants.go
@@ -8,7 +8,12 @@ func main() {
 	const name string = "golang"
 	// name = "javascript" if you give a value to const then it's value will not change and you can declar a value outside the function
 
-	
+	const (
+		port = 5000
+		host = "localhost"
+	)
+
+	fmt.Println(port,host)
 
 	fmt.Println(name)
 	fmt.Println(age)
