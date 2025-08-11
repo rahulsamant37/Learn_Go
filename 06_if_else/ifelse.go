e9817b6b1b738142918baa9e3c7500c8493f1fbf
Rahul Samant <rahulsamantcoc2@gmail.com>
2025-08-11 13:40:06 +0000

    feat: Add teenager age group condition in if-else statement

--- 06_if_else/ifelse.go
+++ 06_if_else/ifelse.go
@@ -3,11 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	age := 18
+	// age := 18
+
+	// if age >= 18 {
+	// 	fmt.Println("person is an adult")
+	// } else {
+	// 	fmt.Println("person is not an adult")
+	// }
+
+	age:=16
 
 	if age >= 18 {
 		fmt.Println("person is an adult")
+	} else if age >= 12 {
+		fmt.Println("preson is a teenager")
 	} else {
-		fmt.Println("person is not an adult")
+		fmt.Println("person is a kid")
 	}
 }
